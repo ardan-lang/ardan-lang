5fe2c08629f53e10185a44ba03f788d41619bf1a
Chidume Nnamdi <kurtwanger40@gmail.com>
2025-10-28 17:53:15 +0100

    [fix]: removed dead code

--- ardan-lang/Compiler/Turbo/TurboChunk.cpp
+++ ardan-lang/Compiler/Turbo/TurboChunk.cpp
@@ -12,20 +12,4 @@ int TurboChunk::addConstant(const Value &v) {
     return (int)constants.size() - 1;
 }
 
-//void Chunk::writeByte(uint8_t b) {
-//    code.push_back(b);
-//}
-//
-//void Chunk::writeUint32(uint32_t v) {
-//    // little endian
-//    code.push_back((v >> 0) & 0xFF);
-//    code.push_back((v >> 8) & 0xFF);
-//    code.push_back((v >> 16) & 0xFF);
-//    code.push_back((v >> 24) & 0xFF);
-//}
-//
-//void Chunk::writeUint8(uint8_t v) {
-//    code.push_back(v);
-//}
-
 size_t TurboChunk::size() const { return code.size(); }
