a2c95277dd1f8404fff3168894718c6ab03bedcb
Chidume Nnamdi <kurtwanger40@gmail.com>
2025-11-01 13:22:38 +0100

    [feat]: added file for interpreter turbo

---
+++ ardan-lang/Interpreter/InterpreterTurbo/InterpreterTurbo.cpp
@@ -0,0 +1,8 @@
+//
+//  InterpreterTurbo.cpp
+//  ardan-lang
+//
+//  Created by Chidume Nnamdi on 01/11/2025.
+//
+
+#include "InterpreterTurbo.hpp"
---
+++ ardan-lang/Interpreter/InterpreterTurbo/InterpreterTurbo.hpp
@@ -0,0 +1,13 @@
+//
+//  InterpreterTurbo.hpp
+//  ardan-lang
+//
+//  Created by Chidume Nnamdi on 01/11/2025.
+//
+
+#ifndef InterpreterTurbo_hpp
+#define InterpreterTurbo_hpp
+
+#include <stdio.h>
+
+#endif /* InterpreterTurbo_hpp */
