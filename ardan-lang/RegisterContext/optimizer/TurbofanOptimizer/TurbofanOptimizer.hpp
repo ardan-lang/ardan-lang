415e22f71348ade5ef8b686519240c2e995433cd
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-05-12 16:32:25 +0100

    feat: added TurboFan optimizer

--- ardan-lang/RegisterContext/optimizer/TurbofanOptimizer/TurbofanOptimizer.hpp
+++ ardan-lang/RegisterContext/optimizer/TurbofanOptimizer/TurbofanOptimizer.hpp
@@ -10,4 +10,22 @@
 
 #include <stdio.h>
 
+class TurbofanOptimizer {
+public:
+    TurbofanOptimizer();
+    
+    IRGraph optimize(IRGraph& inputGraph);
+    
+    void addPass(std::unique_ptr<OptimizationPass> pass, OptimizationPhase phase);
+    void runPhase(OptimizationPhase phase, IRGraph& graph);
+    
+private:
+    std::vector<std::unique_ptr<OptimizationPass>> earlyPasses;
+    std::vector<std::unique_ptr<OptimizationPass>> loopPasses;
+    std::vector<std::unique_ptr<OptimizationPass>> latePasses;
+    std::vector<std::unique_ptr<OptimizationPass>> lastPasses;
+    
+    bool runPasses(std::vector<std::unique_ptr<OptimizationPass>>& passes, IRGraph& graph);
+};
+
 #endif /* TurbofanOptimizer_hpp */
