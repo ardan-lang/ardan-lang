72ebf52455ceebea9084d6805cce29faa7592e8a
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-05-12 12:03:33 +0100

    fix: added IR graph

--- ardan-lang/RegisterContext/ir/IRGraph/IRGraph.hpp
+++ ardan-lang/RegisterContext/ir/IRGraph/IRGraph.hpp
@@ -10,4 +10,27 @@
 
 #include <stdio.h>
 
+class IRGraph {
+public:
+    std::vector<IRNodePtr> nodes;
+    IRNodePtr start;
+    IRNodePtr end;
+    
+    int nextId = 0;
+    
+    IRNodePtr newNode(IRNodeType type, IRValueType vt = IRValueType::Any);
+    IRNodePtr newConstant(Value value);
+    IRNodePtr newParameter(int index);
+    
+    void addEdge(IRNodePtr from, IRNodePtr to, int inputIndex = -1);
+    void addControlEdge(IRNodePtr from, IRNodePtr to);
+    void addEffectEdge(IRNodePtr from, IRNodePtr to);
+    
+    void removeNode(IRNodePtr node);
+    void replaceNode(IRNodePtr oldNode, IRNodePtr newNode);
+    
+    std::vector<IRNodePtr> getReversePostOrder();
+    std::vector<IRNodePtr> getDominators();
+};
+
 #endif /* IRGraph_hpp */
