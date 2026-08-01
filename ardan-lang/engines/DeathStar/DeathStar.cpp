4d3c981892a21646ee5c0dd9b533735314976cb8
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-08-01 12:48:03 +0100

    feat: added DeathStar virtual machine

---
+++ ardan-lang/engines/DeathStar/DeathStar.cpp
@@ -0,0 +1,8 @@
+//
+//  DeathStar.cpp
+//  ardan-lang
+//
+//  Created by Chidume Nnamdi on 01/08/2026.
+//
+
+#include "DeathStar.hpp"
---
+++ ardan-lang/engines/DeathStar/DeathStar.hpp
@@ -0,0 +1,13 @@
+//
+//  DeathStar.hpp
+//  ardan-lang
+//
+//  Created by Chidume Nnamdi on 01/08/2026.
+//
+
+#ifndef DeathStar_hpp
+#define DeathStar_hpp
+
+#include <stdio.h>
+
+#endif /* DeathStar_hpp */
